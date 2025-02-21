b6da5835a4154125a687021a4e02dfd4c727f0c9
Essien Emmanuel <essienemma79@gmail.com>
2025-02-21 10:32:21 -0800

    single selector

--- script.js
+++ script.js
@@ -2280,3 +2280,55 @@ forms.forEach((form) => console.log(form));
 
 console.log(output);
 */
+
+// ------------------------------------ DOM Selector (Single Element)
+
+// document.getElementById()
+
+console.log(document.getElementById("app-title"));
+console.log(document.getElementById("app-title").id);
+console.log(document.getElementById("app-title").getAttribute("id"));
+console.log(document.getElementById("app-title").className);
+
+// Set attribute
+// document.getElementById("app-title").id = "new-id";
+document.getElementById("app-title").title = "Shopping List";
+document.getElementById("app-title").setAttribute("class", "title");
+
+const title = document.getElementById("app-title");
+
+// Get/change context
+console.log(title.textContent);
+title.textContent = "Hello World";
+title.innerText = "Hello Again";
+title.innerHTML = " <strong> Shopping List </strong> ";
+
+// Change Styles
+title.style.color = "red";
+title.style.backgroundColor = "black";
+title.style.padding = "10px";
+title.style.borderRadius = "10px";
+
+// document.querySelector()
+
+console.log(document.querySelector("h1"));
+console.log(document.querySelector("#app-title"));
+console.log(document.querySelector(".container"));
+console.log(document.querySelector('input[type="text"]'));
+console.log(document.querySelector("li:nth-child(2)").innerText);
+
+const secondItem = document.querySelector("li:nth-child(2)");
+secondItem.innerText = "Apple Juice";
+secondItem.style.backgroundColor = "red";
+secondItem.style.borderRadius = "20px 0px";
+secondItem.style.color = "White";
+
+const button = document.querySelector("button");
+button.innerHTML = "<strong> Accept <strong> ";
+
+// Use these methods on other elements
+const list = document.querySelector("ul");
+console.log(list);
+const firstItem = list.querySelector("li");
+firstItem.style.color = "blue";
+
